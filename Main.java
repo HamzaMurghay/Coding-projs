48ea9a2537f9e67be7fb6e534f9a5ebb634f0705
Hamza <hamzamurghay@gmail.com>
2023-09-19 01:13:05 +0530

    Improved Code Readability

--- Main.java
+++ Main.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    int guess = -1,tries = 0;
+    int guess = -1, tries = 0;
     ArrayList guesses = new ArrayList<>();
     static Scanner sc = new Scanner(System.in); //imp, this is how we solved the main prob, NOTOCE HOW THERE IS STATIC
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
 
         Random random = new Random();
         int choice = 6, secretNumber = 1;
@@ -19,7 +20,7 @@ public class Main {
         System.out.println("Before you start you need to choose the difficulty mode that you are going to play on, that is the range from which the 'Secret Number will be chosen!':");
         System.out.println();
 
-        while(choice == 6){
+        while (choice == 6) { 
             System.out.println("---------------------Difficulty Mode---------------------");
             System.out.println("1) Little Bitch Mode [0-10]  (It's called that because if you choose this, you're a little bitch! ;)");
             System.out.println("2) Mere Mortal Mode [0-100]");
@@ -28,33 +29,27 @@ public class Main {
             System.out.println("5) Sigma Mode [0-100,000]");
             choice = sc.nextInt();
 
-            switch(choice){
+            switch (choice) {
                 case 1:
-                secretNumber = random.nextInt(11);
-                break;
-
+                    secretNumber = random.nextInt(11);
+                    break;
                 case 2:
-                secretNumber = random.nextInt(101);
-                break;
-
+                    secretNumber = random.nextInt(101);
+                    break;
                 case 3:
-                secretNumber = random.nextInt(1001);
-                break;
-
+                    secretNumber = random.nextInt(1001);
+                    break;
                 case 4:
-                secretNumber = random.nextInt(10001);
-                break;
-
+                    secretNumber = random.nextInt(10001);
+                    break;
                 case 5:
-                secretNumber = random.nextInt(100001);
-                break;
-
+                    secretNumber = random.nextInt(100001);
+                    break;
                 default:
-                choice = 6;
-                System.out.println("Invalid choice! Please enter a valid choice. Try again");
-                break;
+                    choice = 6;
+                    System.out.println("Invalid choice! Please enter a valid choice. Try again");
+                    break;
             }
-
         }
 
         System.out.println("Ok so here goes, the game has begun! start your gueessing!");
@@ -63,41 +58,32 @@ public class Main {
         Main m = new Main(); //imp, this is how we solved the main prob
         m.CheckGuess(secretNumber); //imp, this is how we solved the main prob
         sc.close();
-    
     }
 
-
-    public void CheckGuess(int SN){ //imp, this is how we solved the main prob NOTICE HOW THERES NO STATIC?
+    public void CheckGuess(int secNum) { //imp, this is how we solved the main prob NOTICE HOW THERES NO STATIC?
             
-            while(guess != SN)
-            {
-                System.out.print("Enter Your Guess! ");
-                guess = sc.nextInt();
+        while (guess != secNum) {
+            System.out.print("Enter Your Guess! ");
+            guess = sc.nextInt();
 
-                if (guesses.contains(guess)){
-                    ;
-                } 
-                else{
-                    guesses.add(guess);
-                    tries++;
-                }
-                
-                if (guess == SN){
-                    System.out.println("Congratulations! You guessed the Secret Number!");
-                    System.out.println("It took you "+ tries +" tries!");
-                    System.out.println("Here are all your tries! ---> "+ guesses);
-                }
-                
+            if (!guesses.contains(guess)) {
+                guesses.add(guess);
+                tries++;
+            }
+            
+            if (guess == secNum) {
+                System.out.println("Congratulations! You guessed the Secret Number!");
+                System.out.println("It took you "+ tries +" tries!");
+                System.out.println("Here are all your tries! ---> "+ guesses);
+            }    
+            else if (guess < secNum) {
+                System.out.println("Too Small!");
+            }
 
-                else if (guess < SN){
-                    System.out.println("Too Small!");
-                }
-
-                else if (guess > SN){
-                    System.out.println("Too Big!");
-                }
+            else if (guess > secNum) {
+                System.out.println("Too Big!");
             }
         }
-
+    }
 
 }
